3943f8bab3bce7a364d33500d0069602b58b389c
Simon Humbert <simal@unpeated.org>
2019-06-23 19:39:22 +0300

    update redrawDisplay(), copy the white pixel surface onto the main surface based on the machine bitmap

--- display.go
+++ display.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "github.com/veandco/go-sdl2/sdl"
     "time"
 )
@@ -14,7 +15,8 @@ const (
 var display [SCREENWIDTH][SCREENHEIGHT]uint8
 
 var window *sdl.Window
-var surface *sdl.Surface
+var surface, pixelSurface *sdl.Surface
+var pixelRect *sdl.Rect
 var black, white uint32
 
 func cleanupDisplay() {
@@ -27,12 +29,30 @@ func initializeDisplay() {
     sdl.Init(sdl.INIT_EVERYTHING)
     window, _ = sdl.CreateWindow("CHIP-8", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, SCREENWIDTH * MAGNIFICATION, SCREENHEIGHT * MAGNIFICATION, sdl.WINDOW_SHOWN)
     surface, _ = window.GetSurface()
+
     black = sdl.MapRGB(surface.Format, 0x00, 0x00, 0x00)
     white = sdl.MapRGB(surface.Format, 0xff, 0xff, 0xff)
+
+    pixelSurface, _ = sdl.CreateRGBSurface(0, MAGNIFICATION, MAGNIFICATION, 8, 0, 0, 0, 0)
+    pixelSurface.FillRect(nil, white)
+
+    pixelRect = new(sdl.Rect)
+    pixelRect.W = MAGNIFICATION
+    pixelRect.H = MAGNIFICATION
 }
 
 func redrawDisplay() {
     surface.FillRect(nil, black)
+
+    for x := 0; x < SCREENWIDTH; x++ {
+        for y := 0; y < SCREENHEIGHT; y++ {
+            if display[x][y] == 1 {
+                pixelRect.X = int32(x * MAGNIFICATION)
+                pixelRect.Y = int32(y * MAGNIFICATION)
+                pixelSurface.Blit(nil, surface, pixelRect)
+            }
+        }
+    }
     window.UpdateSurface()
 }
 
@@ -41,8 +61,8 @@ func runDisplay() {
     defer cleanupDisplay()
 
     redrawDisplay()
-
     for {
         time.Sleep(1 * time.Second)
+        redrawDisplay()
     }
 }
